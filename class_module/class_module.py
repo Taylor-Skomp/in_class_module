2d6cb077a0f2b3a86036b9ed39ee1c4982d6c3a9
Taylor Skomp <tjs5nx@virginia.edu>
2024-07-02 11:15:57 -0400

    creating of class_module package

---
+++ class_module/class_module.py
@@ -0,0 +1,5 @@
+def tell_me_a_joke():
+    '''
+    A function that takes you too literally
+    '''
+    print('a joke')
---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup # type: ignore
+
+setup(
+    name='class_module',
+    version='1.0.0',
+    url='https://github.com/Taylor-Skomp/in_class_module',
+    author='Taylor Skomp',
+    author_email='tjs5nx@virginia.edu',
+    description='A simple package for in-class use.',
+    license = 'GNU V3',
+    packages=['class_module']
+)
